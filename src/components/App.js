3e80704997673b7b756d1ffc67dfeac795b797e1
gdsrosa <gabrieldsrosa@gmail.com>
2019-04-16 20:52:17 -0300

    Change project structure

---
+++ src/components/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './App.module.scss';
+
+const App = () => (
+  <div className={styles.app}>
+    <h1>My site</h1>
+  </div>
+);
+
+export default App;
